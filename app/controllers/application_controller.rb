e902b945f6db345fe9a64943403f542f71d95d1f
tetuya ota <tetu5473@email.com>
2024-04-29 13:33:15 +0900

    2024.04.28

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,20 @@ class ApplicationController < ActionController::Base
 
     protected
 
+    # ユーザーログイン後にリダイレクトするパスを指定
+    def after_sign_in_path_for(resource)
+      users_dash_boards_path # これはダッシュボードへのパスです
+    end
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation])
+    end
+
+    private
+
+    # ユーザーログアウト後にリダイレクトするパスを指定
+    def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path # ログインページへのパス
     end
 end
