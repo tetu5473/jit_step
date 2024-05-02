41ed21963b477f447606348a64ed46f4935e4999
tetuya ota <tetu5473@email.com>
2024-05-03 06:25:43 +0900

    2024/5/2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Deviseのモジュール追加
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_26_124040) do
+ActiveRecord::Schema.define(version: 2024_05_02_205719) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2024_04_26_124040) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
