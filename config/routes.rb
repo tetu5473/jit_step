2fb9819ae803f4e7cf59db9271c2559e2762d602
tetuya ota <tetu5473@email.com>
2024-05-12 10:32:14 +0900

    PR20240512

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
-  
   # 認証関連のルート
   devise_for :users
 
   # ユーザーのダッシュボードページへのルート
   get '/users/dash_boards', to: 'dash_boards#index'
   
-  # アプリケーションのルートページ
-  root to: 'home#index'
+  # アプリケーションのルートページをログイン画面に設定
+  devise_scope :user do
+    authenticated :user do
+      root 'dash_boards#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   # ユーザー関連のルート（newとcreateのみ）
   resources :users, only: [:new, :create, :edit, :update]
+
   # アプリケーションのヘルスチェック用ルート
   get 'up', to: 'rails/health#show', as: :rails_health_check
 
