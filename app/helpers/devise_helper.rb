fed0e44dc89b7038b3a7380acbee924d23d78c12
tetuya ota <tetu5473@email.com>
2024-05-24 06:49:39 +0900

    2024.05.24

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,10 +2,8 @@ module DeviseHelper
   def devise_error_messages!
     return '' if resource.errors.empty?
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t('errors.messages.not_saved',
-                      count: resource.errors.count,
-                      resource: resource.class.model_name.human.downcase)
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, remove_attribute_name(msg)) }.join
+    sentence = I18n.t('errors.messages.not_saved', count: resource.errors.count, resource: resource.class.model_name.human.downcase)
 
     html = <<-HTML
     <div id="error_explanation">
@@ -15,5 +13,12 @@ module DeviseHelper
     HTML
 
     html.html_safe
+  end
+
+  private
+
+  def remove_attribute_name(message)
+    # フィールド名の削除
+    message.gsub(/^Email |^Password |^Name /, '')
   end
 end
