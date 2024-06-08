51f60f1db013d45402b954705e724e83acaa9b26
tetuya ota <tetu5473@email.com>
2024-06-08 22:16:29 +0900

    20240608

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,15 +1,17 @@
 class PasswordsController < Devise::PasswordsController
   def update
     self.resource = resource_class.reset_password_by_token(resource_params)
+    Rails.logger.debug "Password reset errors: #{resource.errors.full_messages.join(", ")}" # デバッグ情報
+
     yield resource if block_given?
 
     if resource.errors.empty?
       flash[:notice] = "パスワードが正常に変更されました。"
       respond_with_navigational(resource) { redirect_to after_resetting_password_path_for(resource) }
     else
-      flash.now[:alert] = "パスワードの変更に失敗しました。"
+      flash.now[:alert] = resource.errors.full_messages.join(", ") # エラーメッセージを詳細に表示
       set_minimum_password_length
-      respond_with_navigational(resource) { render :edit, status: :unprocessable_entity }
+      respond_with_navigational(resource) { render :edit }
     end
   end
 
