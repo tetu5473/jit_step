abf7880b2eacb5274cabdd00c03d35a01cfcc1df
tetuya ota <tetu5473@email.com>
2024-06-17 20:14:28 +0900

    202406171-6修正１

--- db/schema.rb
+++ db/schema.rb
@@ -7,7 +7,7 @@
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
-#
+##
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2024_06_15_110943) do
